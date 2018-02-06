f536e8449248e340099d451a17fd318981445e07
Blocks.Ninja <t.brand@gmail.com>
2018-02-06 17:19:25 +0100

    work 2;

--- src/version.h
+++ src/version.h
@@ -56,4 +56,7 @@ static const int NO_BLOOM_VERSION = 70011;
 //! "sendheaders" command and announcing blocks with headers starts with this version
 static const int SENDHEADERS_VERSION = 70012;
 
+//! DIP0001 was activated in this version
+static const int DIP0001_PROTOCOL_VERSION = 70208;
+
 #endif // BITCOIN_VERSION_H
